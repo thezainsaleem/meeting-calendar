b1999d4171f1199670172b513aac451e940a6bac
Zain Saleem <thezainsaleem@gmail.com>
2021-06-19 15:06:12 +0500

    introduced student devise model with login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  before_action :authenticate_student!
+  before_action :handle_unauthenticated
+ 
+ respond_to :json
+ 
+ private
+ 
+ def handle_unauthenticated
+   unless current_student
+     render json: { error: 'Authentication Error'}, status: 403
+   end
+ end
 end
